87b650cc76546cbcabe38962ff0bfb61633c491f
Chamon562 <Chamon562@gmail.com>
2020-07-17 14:08:02 -0700

    [variables] Add 5 variables

---
+++ app.js
@@ -0,0 +1,39 @@
+//muscle memory of things we did before
+// 5 variables of different data types 
+// first would be string var = string
+// var = number
+//var = array
+// var == boolean
+// var to object (4 different key /value pairs
+// [string], [number], [array], [boolean],)
+
+var string = 'This is my string!'
+
+var num = 1
+
+var array = [1,2,3,4,5,]
+
+true === true 
+
+var things = ['string', 1, [1,2,3], false]
+
+let setence = 'Today, is the Friday!'
+console.log(setnence);
+
+let age = 32;
+console.log(age);
+
+const motorcycles = ['kawasaki', 'yamaha', 'suzuki', 'haybusa'];
+console.log(motorcycles);
+
+let doesThisWork = true;
+console.log(doesThisWork);
+
+const lakers = {
+    headquarters: 'Los Angelos',
+    numberOfPlayers: 11,
+    players: ['LBJ','Anthony Lewis', 'Danny Green'],
+    areChampions: true
+}
+
+console.log(lakers);
